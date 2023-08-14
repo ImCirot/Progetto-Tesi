5786c783fc45fd7a3450703e9adf3eaeacad37b2
ImCirot <ciro.troiano@gmail.com>
2023-08-14 15:14:36 +0200

    Aggiunti commenti esplicativi sulle operazioni effettuate

--- Plotting Data/inital_plot_data.py
+++ Plotting Data/inital_plot_data.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+from codecarbon import EmissionsTracker
 
 # Creazione etichette del dataset
 col_names = [
@@ -71,6 +72,9 @@ X = encoder.transform(X)
 y = label_encoder.transform(y)
 
 # OneShotEncoding della variabile protetta
+# la nuova varibile g conterrà quattro nuove colonne, una per ogni possibile valore della varibile originale.
+# È da notare come, nella descrizione del dataset, i valori possibili siano 5 e l'output che otteniamo dell'encoding sia 4.
+# Questo avviene poichè il valore A95, ovvero donna single, non appare mai nelle 1000 entrate del dataset e dunque non viene considerato.
 ohe_encoder = OneHotEncoder(sparse_output=False)
 
 g = np.asarray(g).reshape(-1,1)
@@ -82,13 +86,24 @@ dataset[features] = X
 dataset['Target'] = y
 
 g_array = np.asarray(g)
-i = 0
+i = 1
 
+# Espandiamo la feature "Personal status and sex" in 4 nuove feature che rappresentano i possibili stati della varibile originale
+# in questo modo otteniamo:
+# Sex_0: A93 -> Uomo single
+# Sex_1: A92 -> Donna divorziata/separata/sposata
+# Sex_2: A91 -> Uomo divorziato/separato
+# Sex_3: A94 -> Uomo sposato/vedovo
+# Come definito prima vengono create solamente 3 nuove feature poichè l'originale viene riutilizzata per l'attributo Sex_0,
+# e non essendo presente alcuna entrata con valore A95 è ridondante creare una feature apposita per quest'ultimo.
 dataset.rename(columns={"Personal status and sex":"Sex_0"}, inplace=True)
 dataset['Sex_0'] = g[:, 0]
-while i < len(g_array):
+while i < 4:
+    dataset.insert(loc=(8+i), column=f"Sex_{i}", value=0)
+    dataset[f'Sex_{i}'] = g[:,i]
     i = i + 1
-    dataset.insert(loc=(8+i), column=f"Sex_{i}", value=g[:, i])
 
-check = ['Sex_0','Sex_1']
+check = ['Sex_0','Sex_1','Sex_2','Sex_3']
+sex3 = dataset.loc[dataset['Sex_3'] == 1.0]
+print(sex3['Sex_3'])
 print(dataset[check])
