91db043afd9bf14541be0acb9e787d0a096231ba
ImCirot <ciro.troiano@gmail.com>
2023-10-14 16:46:52 +0200

    Added new script to format and save new dataset

---
+++ Formatting Data/student_formatting.py
@@ -0,0 +1,20 @@
+import ucimlrepo
+import pandas as pd
+
+def load_dataset():
+    ## funzione di fetch del dataset dalla repository UCI
+
+    # fetch del dataset dalla repo UCI
+    dataset = ucimlrepo.fetch_ucirepo(id=697)
+
+    # estraiamo il dataframe dall'oggetto fetchato
+    df = dataset.data.original
+
+    # rimpiazziamo le classi categoriche della variabile Target con valori numerici
+    df['Target'] = df['Target'].replace('Dropout',0)
+    df['Target'] = df['Target'].replace('Graduate',1)
+
+    # salviamo il dataframe ottenuto in un file csv da utilizzare per i modelli
+    df.to_csv('./Student Dataset/dataset.csv')
+
+load_dataset()
