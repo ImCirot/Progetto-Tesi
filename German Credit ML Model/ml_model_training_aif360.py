2148e9bd75107809ba3ea9954f9730f1b27ef390
ImCirot <ciro.troiano@gmail.com>
2023-10-24 17:18:24 +0200

    Added pickle save to new models

--- German Credit ML Model/ml_model_training_aif360.py
+++ German Credit ML Model/ml_model_training_aif360.py
@@ -172,6 +172,10 @@ def traning_and_testing_model():
     pickle.dump(svm_fair_model_pipeline,open('./output_models/fair_models/svm_aif360_credit_model.sav','wb'))
     pickle.dump(xgb_model_pipeline,open('./output_models/std_models/xgb_aif360_credit_model.sav','wb'))
     pickle.dump(xgb_fair_model_pipeline,open('./output_models/fair_models/xgb_aif360_credit_model.sav','wb'))
+    pickle.dump(post_lr_model_pipeline,open('./output_models/postop_models/lr_aif360_credit_model.sav','wb'))
+    pickle.dump(post_rf_model_pipeline,open('./output_models/postop_models/rf_aif360_credit_model.sav','wb'))
+    pickle.dump(post_svm_model_pipeline,open('./output_models/postop_models/svm_aif360_credit_model.sav','wb'))
+    pickle.dump(post_xgb_model_pipeline,open('./output_models/postop_models/xgb_aif360_credit_model.sav','wb'))
             
 def processing_fairness(dataset,X_set,y_set,protected_features,index):
 
