247ca614fd834f5b364167b257df12c11c5bcc47
ImCirot <ciro.troiano@gmail.com>
2023-11-07 11:51:44 +0100

    Updated script to new training method

--- Bank Marketing ML Model/ml_model_training_std.py
+++ Bank Marketing ML Model/ml_model_training_std.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import *
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -16,9 +16,11 @@ from datetime import datetime
 def load_dataset():
     df = pd.read_csv('./Bank Marketing Dataset/dataset.csv')
 
-    kf = KFold(n_splits=10)
+    for i in range(10):
+        print(f'########################### {i+1} esecuzione ###########################')
+        training_and_testing_models(df)
 
-    df_array = np.asarray(df)
+def training_and_testing_models(df):
 
     feature_names = df.columns.tolist()
 
@@ -32,39 +34,21 @@ def load_dataset():
     svm_model_pipeline = make_pipeline(StandardScaler(),SVC(probability=True))
     xgb_model_pipeline = make_pipeline(StandardScaler(),xgb.XGBClassifier(objective='binary:logistic', random_state=42))
 
-    i = 0
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-    for train_index, test_index in kf.split(df_array):
-        i = i + 1
+    print(f'######### Training modelli #########')
+    lr_model_pipeline.fit(X_train,y_train)
+    rf_model_pipeline.fit(X_train,y_train)
+    svm_model_pipeline.fit(X_train,y_train)
+    xgb_model_pipeline.fit(X_train,y_train)
 
-        print(f'\n######### Inizio {i} iterazione #########\n')
-        
-        X_train = X.iloc[train_index]
-        y_train = y.iloc[train_index]
-        
-        X_test = X.iloc[test_index]
-        y_test = y.iloc[test_index]
+    print(f'######### Testing modelli #########')
+    validate(lr_model_pipeline,'lr',X_test,y_test,True)
+    validate(rf_model_pipeline,'rf',X_test,y_test)
+    validate(svm_model_pipeline,'svm',X_test,y_test)
+    validate(xgb_model_pipeline,'xgb',X_test,y_test)
 
-        lr_model_pipeline.fit(X_train,y_train)
-        rf_model_pipeline.fit(X_train,y_train)
-        svm_model_pipeline.fit(X_train,y_train)
-        xgb_model_pipeline.fit(X_train,y_train)
-
-        validate(lr_model_pipeline,'lr','std',i,X_test,y_test)
-        validate(rf_model_pipeline,'rf','std',i,X_test,y_test)
-        validate(svm_model_pipeline,'svm','std',i,X_test,y_test)
-        validate(xgb_model_pipeline,'xgb','std',i,X_test,y_test)
-
-        print(f'\n######### Fine {i} iterazione #########\n')
-
-    print(f'######### Inizio stesura report finale #########')
-    with open('./reports/final_scores/std/bank_scores.txt','w') as f:
-        f.write(f'LR std model: {str(lr_model_pipeline.score(X,y))}\n')
-        f.write(f'RF std model: {str(rf_model_pipeline.score(X,y))}\n')
-        f.write(f'SVM std model: {str(svm_model_pipeline.score(X,y))}\n')
-        f.write(f'XGB std model: {str(xgb_model_pipeline.score(X,y))}\n')
-
-    print(f'######### Inizio salvataggio modelli #########')
+    print(f'######### Salvataggio modelli #########')
     pickle.dump(lr_model_pipeline,open('./output_models/std_models/lr_bank_model.sav','wb'))
     pickle.dump(rf_model_pipeline,open('./output_models/std_models/rf_bank_model.sav','wb'))
     pickle.dump(svm_model_pipeline,open('./output_models/std_models/svm_bank_model.sav','wb'))
@@ -73,33 +57,24 @@ def load_dataset():
     print(f'######### OPERAZIONI TERMINATE CON SUCCESSO #########')
 
 
-def validate(ml_model,ml_type,ml_vers,index,X_test,y_test):
+def validate(ml_model,ml_type,X_test,y_test,first=False):
     pred = ml_model.predict(X_test)
 
-    matrix = confusion_matrix(y_test,pred)
-
-    report = classification_report(y_test,pred)
+    accuracy = ml_model.score(X_test,y_test)
 
     y_proba = ml_model.predict_proba(X_test)[::,1]
 
     auc_score = roc_auc_score(y_test,y_proba)
 
-    if index == 1:
+    if first:
         open_type = "w"
     else:
         open_type = "a"
 
-    with  open(f"./reports/{ml_vers}_models/bank/{ml_type}_marketing_matrix_report.txt",open_type) as f:
-        f.write(f"{index} iterazione:\n")
-        f.write("Metriche di confusione:\n")
-        f.write(str(matrix))
-        f.write('\n\n')
-
-    with  open(f"./reports/{ml_vers}_models/bank/{ml_type}_marketing_metrics_report.txt",open_type) as f:
-        f.write(f"{index} iterazione:\n")
-        f.write("Metriche di valutazione:")
-        f.write(str(report))
-        f.write(f'\nAUC ROC score: {auc_score}\n')
+    with  open(f"./reports/std_models/bank_metrics_report.txt",open_type) as f:
+        f.write(f"{ml_type}\n")
+        f.write(f"Accuracy: {round(accuracy,3)}")
+        f.write(f'\nROC-AUC score: {round(auc_score,3)}\n')
         f.write('\n')
 
 def print_time(time):
