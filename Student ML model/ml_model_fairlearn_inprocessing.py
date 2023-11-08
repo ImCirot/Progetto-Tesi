79744a7d877f965b0456638e43cb730fb66ceabf
ImCirot <ciro.troiano@gmail.com>
2023-11-08 18:42:29 +0100

    Moved to a 2 minute delay because these scripts are slightly more lighter then others scripts that require 5 mins of delay

--- Student ML model/ml_model_fairlearn_inprocessing.py
+++ Student ML model/ml_model_fairlearn_inprocessing.py
@@ -27,7 +27,7 @@ def load_dataset():
         print_time(elapsed,i)
         if(i < 9):
             print('########################### IDLE TIME START ###########################')
-            sleep(300)
+            sleep(120)
             print('########################### IDLE TIME FINISH ###########################')
 
 @track_emissions(country_iso_code='ITA',offline=True)
